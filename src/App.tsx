be3e95dc31b2dec13bbe284c922ffd6b3d651419
akazwz <akazwz@pm.me>
2021-10-28 15:07:06 +0800

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,101 @@
-import React, {useState} from 'react'
+import React, {FormEvent, useState} from 'react'
 import PixelPic from "./components/pixelpic";
 
 function App() {
-    const [amount, setAmount] = useState(0.3)
+    const [imageSrc, setImageSrc] = useState('')
+    const [pixSrc, setPixSrc] = useState('')
+    const [amount, setAmount] = useState(0.7)
     const [width, setWidth] = useState(500)
     const [height, setHeight] = useState(500)
+
+    const handlePickImage = (event: FormEvent<HTMLInputElement>) => {
+        const files = event.currentTarget.files
+        if (!files) {
+            alert('文件为空')
+            return
+        }
+        const fileObj = files[0]
+        console.log(fileObj.type)
+        if (['jpeg', 'png', 'jpg'].indexOf(fileObj.type.split('/')[1]) < 0) {
+            alert('只能上传图片格式的文件')
+            return
+        }
+        const imageMaxSize = 1024 * 1024 * 5
+        if (fileObj.size > imageMaxSize) {
+            alert('图片最大为5MB')
+            return
+        }
+        const localImageUrl = window.URL.createObjectURL(fileObj)
+        const tempImg = new Image()
+        tempImg.src = localImageUrl
+        tempImg.onload = () => {
+            setWidth(tempImg.width)
+            setHeight(tempImg.height)
+        }
+        setImageSrc(localImageUrl)
+    }
+
     return (
-        <>
+        <div style={{textAlign: 'center'}}>
             <div
                 style={{
-                    textAlign: 'center',
                     display: 'flex',
+                    alignItems: 'center',
                     justifyContent: 'center',
-                    alignContent: 'center'
                 }}
             >
-                <PixelPic
-                    amount={amount}
+                {imageSrc === '' ? <div style={{
+                    width: 200,
+                    height: 200,
+                    backgroundColor: "grey",
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                }}>
+                    <div>
+                        请上传图片
+                    </div>
+                </div> : <PixelPic
+                    amount={1 - amount}
                     width={width}
                     height={height}
-                    imageSrc='https://avatars.githubusercontent.com/u/50396286?v=4'
-                />
-
+                    imageSrc={imageSrc}
+                    getSrc={(src: string) => {
+                        setPixSrc(src)
+                    }} />}
             </div>
-            <div style={{textAlign: 'center'}}>
+            <div>
+                <input
+                    type='file'
+                    accept='image/*'
+                    onInput={handlePickImage}
+                />
+            </div>
+            <div>
                 <input
                     type='range'
                     min={0}
                     max={100}
                     value={amount * 100}
                     onInput={(event) => {
-                        setAmount(Number(event.currentTarget.value) / 100)
+                        setAmount(Math.ceil(Number(event.currentTarget.value)) / 100)
                     }}
                 />
+                <p>
+                    像素化程度: {amount * 100}%
+                </p>
             </div>
-        </>
+            <div>
+                <button onClick={() => {
+                    const a = document.createElement('a')
+                    a.href = pixSrc
+                    a.download = 'pixel.png'
+                    a.click()
+                }}>
+                    保存
+                </button>
+            </div>
+        </div>
     )
 }
 
--- src/components/pixelpic.tsx
+++ src/components/pixelpic.tsx
@@ -1,45 +1,55 @@
 import React, {useEffect, useRef, useState} from 'react'
 
-const PixelPic = (props: { amount: number, imageSrc: string, width: number, height: number }) => {
-    const {amount, imageSrc, width, height} = props
+const PixelPic = (props: { amount: number, imageSrc: string, width: number, height: number, getSrc: any }) => {
+    const {amount, imageSrc, width, height, getSrc} = props
     const imageRef = useRef<HTMLImageElement>(null)
-    const [pixImgSrc, setPixImgSrc] = useState(imageSrc)
+    const [pixImgSrc, setPixImgSrc] = useState('')
 
-    const getPixelImageUrl = () => {
-        const canvas = document.createElement('canvas')
-        canvas.style.display = 'block'
-        canvas.width = width
-        canvas.height = height
-        const ctx = canvas.getContext('2d')
-        if (ctx) {
-            ctx.imageSmoothingEnabled = false
-            const w = width * (amount <= 0 ? 0.01 : amount)
-            const h = height * (amount <= 0 ? 0.01 : amount)
-            const pixelImage = new Image()
-            pixelImage.setAttribute('crossOrigin', 'anonymous')
-            pixelImage.src = imageSrc
-            // 小图
-            ctx.drawImage(pixelImage, 0, 0, w, h)
-            ctx.drawImage(canvas, 0, 0, w, h, 0, 0, width, height)
-            return canvas.toDataURL('image/png')
-        }
-        return ''
-    }
 
     useEffect(() => {
+        const getPixelImageUrl = () => {
+            const canvas = document.createElement('canvas')
+            canvas.style.display = 'block'
+            canvas.width = width
+            canvas.height = height
+            const ctx = canvas.getContext('2d')
+            if (ctx) {
+                ctx.imageSmoothingEnabled = false
+                const w = width * (amount <= 0 ? 0.01 : amount)
+                const h = height * (amount <= 0 ? 0.01 : amount)
+                const pixelImage = new Image()
+                pixelImage.setAttribute('crossOrigin', 'anonymous')
+                pixelImage.src = imageSrc
+                // 小图
+                ctx.drawImage(pixelImage, 0, 0, w, h)
+                const tempCanvas = document.createElement('canvas')
+                tempCanvas.style.display = 'block'
+                tempCanvas.width = width
+                tempCanvas.height = height
+                const tempCtx = tempCanvas.getContext('2d')
+                if (tempCtx) {
+                    tempCtx.drawImage(canvas, 0, 0, w, h, 0, 0, width, height)
+
+                    return tempCanvas.toDataURL('image/png')
+                }
+            }
+            return ''
+        }
         if (imageRef.current) {
             const image = imageRef.current
             if (image.complete) {
                 const url = getPixelImageUrl()
+                getSrc(url)
                 setPixImgSrc(url)
             } else {
                 image.onload = () => {
                     const onloadUrl = getPixelImageUrl()
+                    getSrc(onloadUrl)
                     setPixImgSrc(onloadUrl)
                 }
             }
         }
-    })
+    }, [width, height, amount, imageSrc])
 
     return (
         <div>
