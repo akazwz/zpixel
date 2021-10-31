314d8611f79120fb6f719aadd9a3fbc902953710
zwz <akazwz@pm.me>
2021-10-31 17:34:09 +0800

    add slide

--- src/pages/card.tsx
+++ src/pages/card.tsx
@@ -1,12 +1,51 @@
-import React from 'react'
+import React, { ChangeEvent, useRef, useState } from 'react'
 import CssBaseline from '@mui/material/CssBaseline'
 import Box from '@mui/material/Box'
 import Container from '@mui/material/Container'
+import IconButton from '@mui/material/IconButton'
 import Card from '@mui/material/Card'
 import CardMedia from '@mui/material/CardMedia'
 import ImagePlus from 'pixelarticons/svg/image-plus.svg'
+import Upload from 'pixelarticons/svg/upload.svg'
+import { Button, Divider, Grid, Typography } from '@mui/material'
+import catGift from '../resources/wxpchoua.gif'
+import Slider from '@mui/material/Slider'
+import { VolumeDown } from '@mui/icons-material'
+import Stack from '@mui/material/Stack'
 
 const WideCardMedia = () => {
+  const fileRef = useRef<HTMLInputElement>(null)
+
+  const [srcURL, setSrcURL] = useState('')
+
+  // 上传图片按钮点击
+  const handleBtnUploadClick = () => {
+    if (!fileRef.current) {
+      return
+    }
+    fileRef.current.click()
+  }
+
+  const handleFileInputChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.currentTarget.files
+    if (!files) {
+      alert('文件为空')
+      return
+    }
+    const fileObj = files[0]
+    if (['jpeg', 'png', 'jpg'].indexOf(fileObj.type.split('/')[1]) < 0) {
+      alert('只能上传图片格式的文件')
+      return
+    }
+    const imageMaxSize = 1024 * 1024 * 5
+    if (fileObj.size > imageMaxSize) {
+      alert('图片最大为5MB')
+      return
+    }
+    const localImageUrl = window.URL.createObjectURL(fileObj)
+    setSrcURL(localImageUrl)
+  }
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -16,7 +55,7 @@ const WideCardMedia = () => {
         <Box
           sx={{
             display: 'flex',
-            flexDirection: 'row',
+            flexDirection: 'column',
             p: 1,
             m: 1,
             bgcolor: '#cfe8fc',
@@ -25,26 +64,86 @@ const WideCardMedia = () => {
             alignItems: 'center'
           }}
         >
-          <img
-            width={100}
-            height={100}
-            src={ImagePlus}
-            alt='upload image'
+          <div
+            style={{
+              maxHeight: '60vh',
+              maxWidth: '100%',
+              overflow: 'hidden'
+            }}
+          >
+            {srcURL === '' ?
+              <img
+                width={150}
+                height={150}
+                src={ImagePlus}
+                alt='upload image'
+              />
+              :
+              <img
+                src={srcURL}
+                alt='uploaded image'
+              />
+            }
+          </div>
+          <input
+            id='file'
+            ref={fileRef}
+            type='file'
+            hidden
+            onChange={handleFileInputChange}
           />
+          <label htmlFor='file'>
+            <Button
+              variant='outlined'
+              endIcon={
+                <img
+                  width={30}
+                  height={30}
+                  src={Upload}
+                  alt='upload image'
+                />
+              }
+              sx={{
+                fontFamily: 'zpix',
+                mt: '1vh'
+              }}
+              onClick={handleBtnUploadClick}
+            >
+              UPLOAD
+            </Button>
+          </label>
         </Box>
-        <Box
+        <Grid
+          container
           sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            p: 1,
-            m: 1,
-            bgcolor: '#62a4d9',
-            height: '18vh',
-            justifyContent: 'center',
-            alignItems: 'center'
+            backgroundColor: 'black',
+            color: 'white'
           }}
         >
-        </Box>
+          <Grid item xs={7}>
+            <Stack spacing={2} direction='row' sx={{ mb: 1 }} alignItems='center'>
+              <VolumeDown />
+              <Slider aria-label='Volume' sx={{
+                color: 'white'
+              }} />
+            </Stack>
+            <Stack spacing={2} direction='row' sx={{ mb: 1 }} alignItems='center'>
+              <VolumeDown />
+              <Slider aria-label='Volume' sx={{
+                color: 'white'
+              }} />
+            </Stack>
+            <Stack spacing={2} direction='row' sx={{ mb: 1 }} alignItems='center'>
+              <VolumeDown />
+              <Slider aria-label='Volume' sx={{
+                color: 'white'
+              }} />
+            </Stack>
+          </Grid>
+          <Grid item xs={5}>
+            <img src={catGift} alt='ani cat' />
+          </Grid>
+        </Grid>
         <Box
           sx={{
             display: 'flex',
